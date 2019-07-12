e75736ab0578c0ee96cf52488f98d9ef73db6724
Liz <jesusridesthebus@gmail.com>
2019-07-12 13:37:28 -0700

    try to figure out different syntax with create-react-app file structure

--- src/index.js
+++ src/index.js
@@ -2,21 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-const render = (Component) => {
-  ReactDOM.render(
-    <HashRouter>
-      <Component/>
-    </HashRouter>,
-    document.getElementById('react-app-root')
-  );
-};
+ReactDOM.render(
+  <HashRouter>
+    <Component/>
+  </HashRouter>,
+  document.getElementById('react-app-root'));
 
 render(App);
 
-
-// serviceWorker.unregister();
+serviceWorker.unregister();
